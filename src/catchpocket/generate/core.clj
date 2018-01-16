511474343f5e7537ab5a310ce74fa54664fddc6d
Tim Gilbert <tim@kontor.com>
2018-01-16 18:27:58 -0500

    Backrefs are go

--- src/catchpocket/generate/core.clj
+++ src/catchpocket/generate/core.clj
@@ -29,40 +29,44 @@
 (defn get-ref-type [field {:keys [:stillsuit/datomic-entity-type] :as config}]
   (if-let [override-type (-> field :catchpocket/reference-to datomic/namespace-to-type)]
     (do
-      (log/tracef "Using type %s as referent for field %s" override-type (:attribute/ident field))
+      (log/tracef "Using type %s as return type for field %s" override-type (:attribute/ident field))
       override-type)
     ;; Else no entity found
     (do
-      (log/warnf "No reference type found for field %s, using %s" (:attribute/ident field) datomic-entity-type)
+      (log/warnf "No reference type found for field %s, using %s" (:attribute/ident field)
+                 datomic-entity-type)
       datomic-entity-type)))
 
 (defn get-field-type [field config]
-  (let [field-type   (:attribute/field-type field)
-        lacinia-type (get datomic-to-lacinia field-type)]
+  (let [field-type (:attribute/field-type field)
+        primitive  (get datomic-to-lacinia field-type)]
     (cond
       ;; ASSUMPTION
       (= (:attribute/unique field) :db.unique/identity)
       'ID
 
-      (symbol? lacinia-type)
-      lacinia-type
+      (symbol? primitive)
+      primitive
 
-      (= lacinia-type ::ref)
+      (= primitive ::ref)
       (get-ref-type field config)
 
       :else
-      (log/warnf "Skipping unknown field %s with type %s."
-                 (:attribute/ident field) field-type))))
+      (do
+        (log/warnf "Skipping unknown field %s with type %s."
+                   (:attribute/ident field) field-type)))))
+
 
 (defn make-single-field [field config]
-  (let [{:keys [:attribute/cardinality :attribute/doc]} field
+  (let [{:attribute/keys [cardinality doc]} field
         lacinia-type (get-field-type field config)
         full-type    (if (= cardinality :db.cardinality/many)
                        (list 'list (list 'non-null lacinia-type))
                        lacinia-type)]
     (when lacinia-type
       (merge
-       {:type full-type}
+       {:type    full-type
+        :resolve [:stillsuit.entity-attribute (:attribute/ident field) lacinia-type]}
        (when doc
          {:description doc})))))
 
@@ -115,7 +119,7 @@
 
 (defn find-backrefs
   "Given an entity map, scan it looking for datomic back-references. Return a seq of tuples
-  [from-type field-name to-type datomic-attribute]."
+  [from-type field-name to-type datomic-attribute is-component?]."
   [ent-map config]
   (for [[to-type field-defs] ent-map
         field-def field-defs
@@ -124,23 +128,25 @@
         :let [from-type   (-> field-def :catchpocket/reference-to datomic/namespace-to-type)
               ident       (:attribute/ident field-def)
               datomic-ref (keyword (format "%s/_%s" (namespace ident) (name ident)))]]
-    [from-type backref to-type datomic-ref]))
+    [from-type backref to-type datomic-ref (:attribute/component? field-def)]))
 
 (defn- add-backref
-  [config objects [from-type backref to-type datomic-ref]]
-  (log/tracef "Generating back-reference %s from type %s to type %s for attribute %s"
-              backref from-type to-type datomic-ref)
-  (assoc-in objects [from-type :fields backref]
-            {:type        (list 'list (list 'non-null to-type))
-             :description (format "Back-reference for the `%s` datomic attribute" datomic-ref)}))
+  [config objects [from-type backref to-type datomic-ref is-component?]]
+  (let [plural-type (if is-component?
+                      to-type
+                      (list 'list (list 'non-null to-type)))]
+    (log/tracef "Generating back-reference %s from type %s to type %s for attribute %s"
+                backref from-type plural-type datomic-ref)
+    (assoc-in objects [from-type :fields backref]
+              {:type        plural-type
+               :resolve     [:stillsuit.entity-attribute datomic-ref plural-type]
+               :description (format "Back-reference for the `%s` datomic attribute" datomic-ref)})))
 
 (defn generate-edn [base ent-map config]
   (log/infof "Generating lacinia schema for %d entity types..." (count ent-map))
   (let [objects   (create-objects ent-map config)
         backrefs  (find-backrefs ent-map config)
         decorated (reduce (partial add-backref config) objects backrefs)]
-    ;(log/spy backrefs)
-    ;(log/spy ent-map)
     (assoc base
       :objects decorated
       :catchpocket/generated-at (util/timestamp)
