553c67e96e2b4291dfa6ae39018be24123173fb5
Tim Gilbert <tim@workframe.com>
2018-03-19 13:00:53 -0400

    Support for whitelisting, renaming queries by attribute

--- src/catchpocket/generate/queries.clj
+++ src/catchpocket/generate/queries.clj
@@ -15,20 +15,25 @@
                                :lacinia-type parent-type}]
      :description (format "Find a single %s entity given its `%s` attribute." parent-type ident)}))
 
-(defn- make-query-for
-  [field-type parent-type attr-info config]
-  (let [qname (names/query-name parent-type config)]
-    (log/debugf "Generating top-level query %s from unique attribute %s"
-                qname (:attribute/ident attr-info))
-    [qname (query-definition field-type parent-type attr-info config)]))
-
 (defn attach-queries
   [schema entity-map config]
-  (let [queries (->> (for [[parent-type attr-set] entity-map
-                           attr-info              attr-set
-                           :when (:attribute/unique attr-info)
-                           :let [id         (:attribute/lacinia-name attr-info)
-                                 field-type (get-in schema [:objects parent-type :fields id :type])]]
-                       (make-query-for field-type parent-type attr-info config))
-                     (into {}))]
+  (let [blacklist  (get-in config [:catchpocket/queries :blacklist])
+        whitelist  (get-in config [:catchpocket/queries :whitelist])
+        renames    (get-in config [:catchpocket/queries :names])
+        query-defs (for [[parent-type attr-set] entity-map
+                         attr-info attr-set
+                         :when (:attribute/unique attr-info)
+                         :let [id         (:attribute/lacinia-name attr-info)
+                               attr-ident (:attribute/ident attr-info)
+                               field-type (get-in schema [:objects parent-type :fields id :type])
+                               qname      (or (get renames attr-ident)
+                                              (names/query-name parent-type config))]
+                         :when (or (nil? whitelist) (contains? whitelist qname))
+                         :when (or (nil? blacklist) (not (contains? blacklist qname)))
+                         :let [qdef (query-definition field-type parent-type attr-info config)]]
+                     (do
+                       (log/debugf "Generating top-level query %s from unique attribute %s"
+                                   qname attr-ident)
+                       [qname qdef]))
+        queries    (into {} query-defs)]
     (update schema :queries merge queries)))
