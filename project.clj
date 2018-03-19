0da0f9c4761d02b0eaf562050ac018b3d652bbdc
Tim Gilbert <tim@workframe.com>
2018-03-19 17:21:39 -0400

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.workframe/catchpocket "0.2.0-SNAPSHOT"
+(defproject com.workframe/catchpocket "0.2.0"
   :description "datomic-to-lacinia schema extractor"
   :url "https://github.com/workframers/catchpocket"
   :pedantic? :warn
