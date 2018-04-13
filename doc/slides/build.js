2d5f36b574ced4bbb4699bdbfac6e83a047d2a48
Tim Gilbert <tim@workframe.com>
2018-04-13 16:14:00 -0400

    Slide building looks ok

--- doc/slides/build.js
+++ doc/slides/build.js
@@ -4,5 +4,5 @@ require('asciidoctor-reveal.js');
 
 // Convert the document 'presentation.adoc' using the reveal.js converter
 var attributes = {'revealjsdir': 'node_modules/reveal.js@'};
-var options = {safe: 'safe', backend: 'revealjs', attributes: attributes};
+var options = {safe: 'unsafe', backend: 'revealjs', attributes: attributes, to_dir: '../../target/slides'};
 asciidoctor.convertFile('index.adoc', options);
